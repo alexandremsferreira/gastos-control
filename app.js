a679c039c1e2a40e291cadd15c0caf9de2e33587
alexandremsferreira <alexandremsferreira1998@gmail.com>
2025-03-10 21:51:32 -0300

    data de vencimento, lançamento e date-fns

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const expensiveList = document.getElementById('expensive-list')
 const addBtn = document.getElementById('addBtn')
 const inputConta = document.getElementById('conta')
+const inputVencimento = document.getElementById('vencimento')
 const inputCategoria = document.getElementById('categoria')
 const inputValor = document.getElementById('valor')
 const totalGastos = document.getElementById('totalGastos')
@@ -9,26 +10,36 @@ let data = []
 
 function getInputValues() {
     // Verifica se todos os campos estão preenchidos
-    const contaValida = inputConta.value.trim() !== ""
-    const categoriaValida = inputCategoria.value.trim() !== ""
-    const valorValido = inputValor.value.trim() !== "" && !isNaN(inputValor.value)
+    const contaValida = inputConta.value.trim() !== "";
+    const vencimentoValido = inputVencimento.value.trim() !== "";
+    const categoriaValida = inputCategoria.value.trim() !== "";
+    const valorValido = inputValor.value.trim() !== "" && !isNaN(inputValor.value);
 
-    if (contaValida && categoriaValida && valorValido) {
+    if (contaValida && vencimentoValido && categoriaValida && valorValido) {
+        const vencimentoFormatado = dateFns.format(
+            new Date(inputVencimento.value), // Converte para Date
+            "DD/MM/YYYY" // Formata para o padrão brasileiro
+        );
+
         const newExpense = {
             id: Date.now(),
             title: inputConta.value.trim(),
+            ticketExpiration: vencimentoFormatado, // Data de vencimento formatada
             category: inputCategoria.value.trim(),
+            date: dateFns.format(new Date(), "DD/MM/YYYY"), // Data atual formatada
             amount: parseFloat(inputValor.value).toFixed(2)
-        }
+        };
 
-        data.push(newExpense)
-        inputConta.value = ''
-        inputCategoria.value = ''
-        inputValor.value = ''
+        data.push(newExpense);
+        inputConta.value = "";
+        inputVencimento.value = "";
+        inputCategoria.value = "";
+        inputValor.value = "";
     } else {
-        alert('Preencha todos os campos corretamente!')
+        alert("Preencha todos os campos corretamente!");
     }
 }
+
 
 function renderExpensiveList() {
     expensiveList.innerHTML = ''
@@ -40,6 +51,8 @@ function renderExpensiveList() {
                     <i class="text-yellow-400">${index + 1}</i>
                     <div>
                         <div class="font-bold">${item.title}</div>
+                        <div class="text-xs">Data de vencimento: ${item.ticketExpiration}</div>
+                        <div class="text-xs">Data de lançamento: ${item.date}</div>
                         <div class="text-sm">${item.category} - R$ ${item.amount}</div>
                     </div>
                 </div>
